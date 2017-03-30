abcbfcc03e8e92c2e3c82c55d0366d1ec3d07ce4
han <895240740@qq.com>
2017-03-30 20:47:36 +0800

    hanxin

--- app/src/main/java/com/example/a89524/lesson1/LifeActivity.java
+++ app/src/main/java/com/example/a89524/lesson1/LifeActivity.java
@@ -1,13 +1,55 @@
 package com.example.a89524.lesson1;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class LifeActivity extends AppCompatActivity {
+public class LifeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textview;
+    private ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
+
+        textview = (TextView) findViewById(R.id.text_1);
+        bar = (ProgressBar) findViewById(R.id.progressbar_1);
+
+        textview.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_1:
+                if (bar.getVisibility() == View.VISIBLE) {
+                    bar.setVisibility(View.GONE);
+                } else if (bar.getVisibility() == View.GONE) {
+                    bar.setVisibility(View.VISIBLE);
+                }
+              break;
+            case R.id.btn3:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(LifeActivity.this);
+                dialog.setTitle("this is a dialog");
+                dialog.setMessage("1");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v("dialog", "here");
+                    }
+                });
+                dialog.show();
+                break;
+            default:
+              break;
+        }
     }
 }
