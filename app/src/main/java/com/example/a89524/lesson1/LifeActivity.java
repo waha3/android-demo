bc74c064a7aae18e6b8a2902b9332960fde5cc42
waha3 <895240740@qq.com>
2017-03-31 01:53:17 +0800

    hanxin

--- app/src/main/java/com/example/a89524/lesson1/LifeActivity.java
+++ app/src/main/java/com/example/a89524/lesson1/LifeActivity.java
@@ -1,11 +1,13 @@
 package com.example.a89524.lesson1;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -13,6 +15,8 @@ public class LifeActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView textview;
     private ProgressBar bar;
+    private Button btn;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,12 @@ public class LifeActivity extends AppCompatActivity implements View.OnClickListe
 
         textview = (TextView) findViewById(R.id.text_1);
         bar = (ProgressBar) findViewById(R.id.progressbar_1);
+        btn = (Button) findViewById(R.id.btn3);
+        btn1 = (Button) findViewById(R.id.btn4);
 
         textview.setOnClickListener(this);
+        btn.setOnClickListener(this);
+        btn1.setOnClickListener(this);
     }
 
     @Override
@@ -36,10 +44,11 @@ public class LifeActivity extends AppCompatActivity implements View.OnClickListe
                 }
               break;
             case R.id.btn3:
+                Log.d("log", "here");
                 AlertDialog.Builder dialog = new AlertDialog.Builder(LifeActivity.this);
                 dialog.setTitle("this is a dialog");
                 dialog.setMessage("1");
-                dialog.setCancelable(false);
+                dialog.setCancelable(true);
                 dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -48,6 +57,12 @@ public class LifeActivity extends AppCompatActivity implements View.OnClickListe
                 });
                 dialog.show();
                 break;
+            case R.id.btn4:
+                ProgressDialog progressdialog = new ProgressDialog(LifeActivity.this);
+                progressdialog.setTitle("this is progressdialog");
+                progressdialog.setMessage("hello world");
+                progressdialog.setCancelable(true);
+                progressdialog.show();
             default:
               break;
         }
